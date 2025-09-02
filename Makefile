91eeb4b4ea341301c74516e4d951e8e6016016ae
Pukar Kafle <pukar@smartcontents.com.np>
2025-09-02 17:02:01 +0545

    project setup

---
+++ Makefile
@@ -0,0 +1,30 @@
+help:
+	@echo
+	@echo "install                            -- install backend dependencies"
+	@echo "lint                               -- lint backend"
+	@echo "format                             -- format backend"
+	@echo "mypy                               -- type check backend"
+	@echo "dev                                -- start backend development server"
+	@echo
+
+
+.PHONY: install
+install:
+	uv sync --frozen
+
+.PHONY: lint
+lint:
+	uv run ruff check .
+
+.PHONY: mypy
+mypy:
+	uv run mypy .
+
+.PHONY: format
+format:
+	uv run ruff check --fix .
+	uv run ruff format .
+
+.PHONY: dev
+dev:
+	uv run uvicorn main:app --reload --host "127.0.0.1" --port 8080 --workers 1 --log-level info
---
+++ settings.py
@@ -0,0 +1,42 @@
+from pydantic import field_validator
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
+
+    # Application settings
+    ENV: str = "local"
+    DEBUG: int = 0
+
+    # Gunicorn settings
+    GUNICORN_WORKERS: int = 1
+    GUNICORN_THREADS: int = 8
+    GUNICORN_ACCESS_LOG: str = "-"
+    GUNICORN_ERROR_LOG: str = "-"
+
+    @field_validator("DEBUG", mode="before")
+    @classmethod
+    def validate_debug(cls, v: str) -> int:
+        if int(v) in [0, 1]:
+            return int(v)
+        raise ValueError("DEBUG must be 0 or 1")
+
+    @field_validator("GUNICORN_WORKERS", mode="before")
+    @classmethod
+    def validate_gunicorn_workers(cls, v: str) -> int:
+        if int(v) > 0:
+            return int(v)
+        raise ValueError("GUNICORN_WORKERS must be a positive integer")
+
+    @field_validator("GUNICORN_THREADS", mode="before")
+    @classmethod
+    def validate_gunicorn_threads(cls, v: str) -> int:
+        if int(v) > 0:
+            return int(v)
+        raise ValueError("GUNICORN_THREADS must be a positive integer")
+
+
+settings = Settings()
